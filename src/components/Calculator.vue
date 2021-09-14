a6983b99d7eedcefa9bdc393199f0b847d368586
hadi <hadi_960@hotmail.com>
2021-09-14 12:09:38 -0500

    parseFlaot logic

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -6,19 +6,19 @@
   <div class="btn" @click="append(7)">7</div>
   <div class="btn" @click="append(8)">8</div>
   <div class="btn" @click="append(9)">9</div>
-  <div class="btn operator">/</div>
+  <div class="btn operator" @click="divide">/</div>
   <div class="btn" @click="append(4)">4</div>
   <div class="btn" @click="append(5)">5</div>
   <div class="btn" @click="append(6)">6</div>
-  <div class="btn operator">x</div>
+  <div class="btn operator" @click="divide">x</div>
   <div class="btn" @click="append(1)">1</div>
   <div class="btn" @click="append(2)">2</div>
   <div class="btn" @click="append(3)">3</div>
-  <div class="btn operator">-</div>
+  <div class="btn operator" @click="subtract">-</div>
   <div class="btn" @click="append(0)">0</div>
-  <div class="btn">.</div>
-  <div class="btn">=</div>
-  <div class="btn operator">+</div>
+  <div class="btn" @click="dot">.</div>
+  <div class="btn" @click="equal">=</div>
+  <div class="btn operator" @click="sum">+</div>
   </div>
 </template>
 
@@ -26,7 +26,10 @@
 export default {
     data() {
         return {
-            result: '0'
+            result: '0',
+            prev: null,
+            operator: null,
+            operatorClick: false
         }
     },
     methods: {
@@ -37,7 +40,43 @@ export default {
         this.result = parseFloat(this.result)/100;
     },
     append(number) {
+        if(this.operatorClick){
+            this.result = '';
+            this.operatorClick = false;
+        }
         this.result = this.result+number;
+    },
+    dot() {
+        if(this.result.indexOf('.') === -1) {
+            this.append('.');
+        }
+    },
+    divide() {
+        this.operator = (a,b) => b/a; 
+        this.setPrev();
+    },
+    multiply() {
+        this.operator = (a,b) => a*b;
+        this.setPrev();
+    },
+    subtract() {
+        this.operator = (a,b) => a-b;
+        this.setPrev();
+    },
+    sum() {
+        this.operator = (a,b) => a+b;
+        this.setPrev();
+    },
+    equal() {
+        this.result = this.operator(
+            parseFloat(this.result),
+            parseFloat(this.prev)
+        );
+        this.prev=null;
+    },
+    setPrev() {
+        this.prev= this.result;
+        this.operatorClick = true;
     }
   }
 }
